34edf6cd844ff9c35dfd06a951a2272f00bc48a8
WELCE <3245185172@qq.com>
2021-11-26 17:16:11 +0800

    diary
    日记翻页
    bug:要改变翻页方向要按两次键位

---
+++ unityproject/Assets/DialogSystem.cs
@@ -0,0 +1,78 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+public class DialogSystem : MonoBehaviour
+{
+    public Text textLabel;
+    public Image faceImage;
+
+    public TextAsset textFile;
+    public int index;
+
+    List<string> textList = new List<string>();
+
+    // Start is called before the first frame update
+    void Awake()
+    {
+        GetTextFromFile(textFile);//获得txt文件
+        //index = 0;
+    }
+    void GetTextFromFile(TextAsset file)
+    {
+
+
+        textList.Clear();
+        index = 0;
+
+        var lineData = file.text.Split('\n');//按行切割，变成一个数组
+        foreach (var line in lineData)
+        {
+            textList.Add(line);
+        }
+
+    }
+
+
+
+    private void OnEnable()//Onenable在Start前调用,Awake后
+    {
+        textLabel.text = textList[index];//输入第一行
+        index++;
+    }
+
+
+
+    // Update is called once per frame
+    void Update()
+    {
+
+
+        if (Input.GetKeyDown(KeyCode.C))
+        {
+            textLabel.text = textList[index];
+            index++;
+
+        }
+        if(Input.GetKeyDown(KeyCode.Z) && index>0)
+        {
+            --index;
+            textLabel.text = textList[index];
+            
+        }
+
+        if (Input.GetKeyDown(KeyCode.D) && index == textList.Count)
+        {
+            gameObject.SetActive(false);
+            index = 0;
+            return;
+        }
+
+
+    }
+
+
+
+}
